b041f38865cfe3b9dd9c7ca6234a7cca804915f9
Franco Acebo Canal <francoacebocanal@gmail.com>
2021-01-18 16:18:51 -0300

    commit

---
+++ MyFirstProject/Assets/_Scripts/MyFirstScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    //Debug.Log() sirve para poner algo en la consola
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+     }
+}
+ 
